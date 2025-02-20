22aa52a838a92daf0d20708bafed9d1722dadd3b
IVELINYTBG1 <ivelinbonev446@gmail.com>
2025-02-20 23:24:56 +0200

    My unfinished minigame

---
+++ MyFirstMiniGame/MyFirstMiniGame/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace MyFirstMiniGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int RandomNum = random.Next(1, 100); 
+            int UserGuess = int.Parse(Console.ReadLine());
+            if (UserGuess == RandomNum)
+            {
+                Console.WriteLine("You guessed the right number!");
+            }
+            else if (UserGuess < RandomNum)
+            {
+                while (UserGuess < RandomNum)
+                    Console.WriteLine("aim lower");
+            }
+            else
+            {
+                while (UserGuess > RandomNum)
+                    Console.WriteLine("aim hier");
+            }
+        }
+    }
+}
